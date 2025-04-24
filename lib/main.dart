0c2c4945c2bdd66ed342c6edf49c8d808e6e2445
Harmony Teng <hteng1@lsu.edu>
2025-04-24 01:00:22 -0500

    update with background music and modify login and main screen

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,9 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:audioplayers/audioplayers.dart';
-import 'audio.dart';
+import 'screens/audio.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:animate_do/animate_do.dart';
 
 
 Future<void> main() async {
@@ -46,6 +48,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Liar\'s Bar',
       theme: ThemeData(
+        textTheme: GoogleFonts.pressStart2pTextTheme(), // change the font to match style
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.brown,
           brightness: Brightness.dark,
@@ -155,99 +158,104 @@ class HomePage extends StatelessWidget {
                         ),
                       ),
                       const SizedBox(height: 20),
-                      const Text(
-                        'A roll of the dice. A twist of the truth.',
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: Color.fromARGB(255, 195, 187, 187),
-                          fontStyle: FontStyle.italic,
+                      FadeInDown(
+                        delay: const Duration(milliseconds: 500),
+                        child: const Text(
+                          'A roll of the dice. A twist of the truth.',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Color.fromARGB(255, 195, 187, 187),
+                            fontStyle: FontStyle.italic,
+                          ),
                         ),
                       ),
                       const SizedBox(height: 20),
-                      Container(
-                        decoration: BoxDecoration(
-                          color: const Color.fromRGBO(33, 17, 0, 0.8),
-                          borderRadius: BorderRadius.circular(20),
-                          border: Border.all(
-                            color: Colors.amber.shade800,
-                            width: 2,
+                      ZoomIn(
+                        delay: const Duration(milliseconds: 1000),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: const Color.fromRGBO(33, 17, 0, 0.8),
+                            borderRadius: BorderRadius.circular(20),
+                            border: Border.all(
+                              color: Colors.amber.shade800,
+                              width: 2,
+                            ),
+                            boxShadow: const [
+                              BoxShadow(
+                                color: Color.fromRGBO(0, 0, 0, 0.3),
+                                blurRadius: 10,
+                                offset: Offset(0, 5),
+                              ),
+                            ],
                           ),
-                          boxShadow: const [
-                            BoxShadow(
-                              color: Color.fromRGBO(0, 0, 0, 0.3),
-                              blurRadius: 10,
-                              offset: Offset(0, 5),
-                            ),
-                          ],
-                        ),
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 40,
-                          vertical: 20,
-                        ),
-                        child: Column(
-                          children: [
-                            ElevatedButton(
-                              onPressed: () async {
-                                //play background music when enter the bar
-                                await AudioManager().player.setSource(AssetSource('sound/back.mp3'));
-                                await AudioManager().player.setReleaseMode(ReleaseMode.loop);
-                                await AudioManager().player.resume();
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => const GameSelectionPage(),
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 40,
+                            vertical: 20,
+                          ),
+                          child: Column(
+                            children: [
+                              ElevatedButton(
+                                onPressed: () async {
+                                  await AudioManager().player.setSource(AssetSource('sound/back2.mp3'));
+                                  await AudioManager().player.setReleaseMode(ReleaseMode.loop);
+                                  await AudioManager().player.resume();
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => const GameSelectionPage(),
+                                    ),
+                                  );
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.amber,
+                                  foregroundColor: Colors.brown.shade900,
+                                  padding: const EdgeInsets.symmetric(
+                                    horizontal: 50,
+                                    vertical: 15,
                                   ),
-                                );
-                              },
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor: Colors.amber,
-                                foregroundColor: Colors.brown.shade900,
-                                padding: const EdgeInsets.symmetric(
-                                  horizontal: 50,
-                                  vertical: 15,
-                                ),
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(20),
-                                ),
-                              ),
-                              child: const Text(
-                                'Enter the Bar',
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                            const SizedBox(height: 20),
-                            ElevatedButton(
-                              onPressed: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => const Instruction(),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20),
                                   ),
-                                );
-                              },
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor: Colors.amber,
-                                foregroundColor: Colors.brown.shade800,
-                                padding: const EdgeInsets.symmetric(
-                                  horizontal: 50,
-                                  vertical: 15,
                                 ),
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(20),
+                                child: const Text(
+                                  'Enter the Bar',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  ),
                                 ),
                               ),
-                              child: const Text(
-                                'Instructions',
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
+                              const SizedBox(height: 20),
+                              ElevatedButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => const Instruction(),
+                                    ),
+                                  );
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.amber,
+                                  foregroundColor: Colors.brown.shade800,
+                                  padding: const EdgeInsets.symmetric(
+                                    horizontal: 50,
+                                    vertical: 15,
+                                  ),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                ),
+                                child: const Text(
+                                  'Instructions',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  ),
                                 ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       ),
                     ],
